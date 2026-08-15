3ff72324eb3cd28fd6140242a2ec2668a407c33d
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-08-15 18:46:30 +0530

    fix: Update Mailer configuration to use Gmail SMTP settings and remove Brevo references

--- utils/Mailer.js
+++ utils/Mailer.js
@@ -1,23 +1,15 @@
-const nodemailer = require("nodemailer");
-
-console.log("Mail service starting...");
-console.log("BREVO USER exists:", !!process.env.BREVO_SMTP_USER);
-console.log("BREVO KEY exists:", !!process.env.BREVO_SMTP_KEY);
-console.log("EMAIL exists:", !!process.env.EMAIL);
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+host:"smtp.gmail.com",
+port:587,
+secure:true,
+auth:{
+user:process.env.EMAIL,
+pass:process.env.EMAIL_PASSWORD
 
-  auth: {
-    user: process.env.BREVO_SMTP_USER,
-    pass: process.env.BREVO_SMTP_KEY,
-  },
-
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
-});
+}
+})
 
 module.exports = transporter;
