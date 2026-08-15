f230a97bfd48bcb59cd1205956c2d4d1fab075c4
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-08-15 18:01:40 +0530

    fix: Update Mailer configuration for Gmail service and improve timeout settings

--- utils/Mailer.js
+++ utils/Mailer.js
@@ -1,15 +1,16 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-host:"smtp.gmail.com",
-port:587,
-secure:false,
-auth:{
-user:process.env.EMAIL,
-pass:process.env.EMAIL_PASSWORD
+  service: "gmail",
 
-}
-})
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+});
 
 module.exports = transporter;
