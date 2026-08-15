7474e1efb3fd68c4277a92b4ab73570797e6ed24
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-08-15 19:11:22 +0530

    fix: Update Mailer configuration to use port 465 and ensure secure connection

--- utils/Mailer.js
+++ utils/Mailer.js
@@ -1,17 +1,19 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
 const dns = require("dns");
 
 dns.setDefaultResultOrder("ipv4first");
-const transporter = nodemailer.createTransport({
-host:"smtp.gmail.com",
-port:587,
-secure:true,
-auth:{
-user:process.env.EMAIL,
-pass:process.env.EMAIL_PASSWORD
 
-}
-})
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+
+  connectionTimeout: 30000,
+});
 
 module.exports = transporter;
