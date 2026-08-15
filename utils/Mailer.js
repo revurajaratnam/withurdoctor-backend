c16e8b3bf8215c8d55b0dba501e4beb986164923
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-08-15 19:16:43 +0530

    fix: Remove deprecated DNS result order setting and ensure IPv4 usage in Mailer configuration

--- utils/Mailer.js
+++ utils/Mailer.js
@@ -1,8 +1,6 @@
 const nodemailer = require("nodemailer");
 const dns = require("dns");
 
-dns.setDefaultResultOrder("ipv4first");
-
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
@@ -13,7 +11,14 @@ const transporter = nodemailer.createTransport({
     pass: process.env.EMAIL_PASSWORD,
   },
 
+  // Force Gmail SMTP to use IPv4
+  lookup: (hostname, options, callback) => {
+    dns.lookup(hostname, { family: 4 }, callback);
+  },
+
   connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 module.exports = transporter;
